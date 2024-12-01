2d0e8c9f8838c679c62b9d10d13653d7f5e41cc8
AreOks <aoksana@yandex.ru>
2024-12-01 14:21:15 +0300

    Добавляет адаптивную сетку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+
+const icons = document.querySelectorAll('.toggle');
+icons.forEach ((icon) => {
+  icon.addEventListener('click', (event) => {
+    icon.classList.toggle('open');
+  });
+});
