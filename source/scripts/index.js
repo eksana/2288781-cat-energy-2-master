fc661446c9de7a44d23dd4b3d0b9191ef0604f20
AreOks <aoksana@yandex.ru>
2024-12-01 22:35:15 +0300

    Отправляет

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,33 @@
 /* в этот файл добавляет скрипты*/
 
-const icons = document.querySelectorAll('.toggle');
-icons.forEach ((icon) => {
-  icon.addEventListener('click', (event) => {
-    icon.classList.toggle('open');
-  });
-});
+// ползунок
+
+document.documentElement.lang = 'ru';
+document.documentElement.classList.add('page');
+document.body.classList.add('page__body');
+
+const slider = document.querySelector('.slider');
+const curtain = slider.querySelector('.stats__range');
+const sliderStyles = getComputedStyle(slider);
+let curtainPlaceStart;
+let clientX;
+
+window.addEventListener('pointerup', stopTheCurtainShifting);
+curtain.addEventListener('pointerdown', startTheCurtainShifting);
+
+function startTheCurtainShifting (event) {
+  curtainPlaceStart = Number(sliderStyles.getPropertyValue('--curtain-place'));
+  clientX = event.clientX;
+  window.addEventListener('pointermove', shiftТheСurtain);
+}
+
+function shiftТheСurtain (event) {
+  const deltaX = event.clientX - clientX;
+  const cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth;
+  const curtainPlace = Math.min(Math.max(cursorPlace, 0), 1);
+  slider.style.setProperty('--curtain-place', `${curtainPlace}`);
+}
+
+function stopTheCurtainShifting () {
+  window.removeEventListener('pointermove', shiftТheСurtain);
+}
